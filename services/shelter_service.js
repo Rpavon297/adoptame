cb33d712b3059dfa1ef7d4707f390f29c5b5527d
rpavon297 <rpavon@ucm.es>
2020-05-20 20:51:09 +0200

    ya se pueden crear cuentas de protectora

--- services/shelter_service.js
+++ services/shelter_service.js
@@ -3,10 +3,29 @@ class ShelterService {
         this.pool = pool;
     }
 
+    
+
+    // Devuelve todas las solucitudes pendientes
+    getRequests(callback){
+        this.pool.getConnection((err, connection) =>{
+            if(err){
+                callback(err);
+                return;
+            }
+            connection.query("select * from shelterRequest where currentStatus = 'pending'", (err, result) => {
+                if (err) { callback(err); return; }
+                connection.release();
+                callback(null, result)
+            });
+        });   
+    }
+
     // Crea una petición de afiliación con los datos de la protectora, o si ya existe una petición rechazada, la reabre
     createRequest(request, callback) {
+        var forcedkeys = ["shelterName", "shelterAddress", "forename", "surenames", "email", "pass"];
+
         for (var key in request) {
-            if (key != "img" && (request[key] === "" || request[key] === undefined)) {
+            if (forcedkeys.includes(key)  && (request[key] === "" || request[key] === undefined)) {
                 console.log("campo a null:");
                 console.log(key);
                 callback(null, false); return;
@@ -23,7 +42,7 @@ class ShelterService {
                 if (result.length == 0) {
                     connection.query(
                         "insert into shelterRequest(email,pass,forename,surnames,birthdate, tlf, shelterName, shelterAddress, webpage, shelterDescription, currentStatus) values (?,?,?,?,?,?,?,?,?,?,?)",
-                        [request.email, request.password, request.name, request.surname, request.jqueryDate, request.tlf, request.shelter_name, request.location, request.web, request.descripcion, "pending"],
+                        [request.email, request.pass, request.forename, request.surnames, request.birthdate, request.tlf, request.shelterName, request.shelterAddress, request.webpage, request.shelterDescription, "pending"],
                         (err) => {
                             if (err) {
                                 callback(err);
@@ -57,24 +76,18 @@ class ShelterService {
                     );
                 }
                 else { connection.release(); callback(null, false); }
-            })
+            });
         });
     }
 
     // Si existe una petición vinculada al email, pone su estatus como rechazada
     rejectRequest(email, callback){
-        for (var key in request) {
-            if (key != "img" && (request[key] === "" || request[key] === undefined)){
-                callback(null, false); return;
-            }
-        }
-
         this.pool.getConnection((err, connection) => {
             if (err) {
                 callback(err);
                 return;
             }
-            connection.query("select email from account where email = ?", [request.email], (err, result) => {
+            connection.query("select * from shelterRequest where email = ?", [request.email], (err, result) => {
                 if (err) { callback(err); return; }
                 if (result.length > 0) {
                     connection.query(
@@ -100,19 +113,12 @@ class ShelterService {
 
     // Si existe una petición vinculada al email, pone su estatus como aceptada
     acceptRequest(email, callback){
-        console.log("accepted request");
-        for (var key in request) {
-            if (key != "img" && (request[key] === "" || request[key] === undefined)) {
-                callback(null, false); return;
-            }
-        }
-
         this.pool.getConnection((err, connection) => {
             if (err) {
                 callback(err);
                 return;
             }
-            connection.query("select email from account where email = ?", [request.email], (err, result) => {
+            connection.query("select * from shelterRequest where email = ?", [email], (err, result) => {
                 if (err) { callback(err); return; }
                 if (result.length > 0) {
                     connection.query(
@@ -125,7 +131,7 @@ class ShelterService {
                             }
                             else {
                                 connection.release();
-                                callback(null, true);
+                                callback(null, result[0]);
                                 return;
                             }
                         }
